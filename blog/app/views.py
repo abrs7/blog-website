dfd242a048d099f180b325f7e178e538a0039110
abrs7 <abrishsambong@gmail.com>
2024-03-04 01:08:21 +0300

    CREATE models.py

--- blog/app/views.py
+++ blog/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contact(request):
+    message = {'address':'Bole, Addis Ababa'}
+    return render(request,'contact.html',context=message)
